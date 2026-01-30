707b555e82de1ece4977f3ade189380a7dcbb16b
ALaborate <kosia4enko.g@gmail.com>
2026-01-30 22:21:34 +0200

    Stable glider

--- Assets/Scripts/PaperPlane.cs
+++ Assets/Scripts/PaperPlane.cs
@@ -4,11 +4,12 @@ using UnityEngine.InputSystem;
 
 public class PaperPlane : MonoBehaviour
 {
-    const float MAX_SPEED = 48;
+    const float MAX_SPEED = 24;
 
     [SerializeField] float stabilizerStrength = 1;
     [SerializeField] AnimationCurve aileronTorqueWrtSpeed = AnimationCurve.EaseInOut(0, 0, MAX_SPEED, 4);
     [SerializeField] AnimationCurve liftWrtSpeed = AnimationCurve.EaseInOut(0, 0, MAX_SPEED, 4);
+    [SerializeField] AnimationCurve linearDumpWrtAngularVelocity = AnimationCurve.EaseInOut(0, .2f, .5f, .54f);
 
     InputActionMap actionMap;
     //InputAction iMove;
@@ -40,6 +41,11 @@ public class PaperPlane : MonoBehaviour
         var forwardVelocity = Vector3.Project(rb.linearVelocity, transform.forward);
         state.fwdSpeed = forwardVelocity.magnitude;
 
+        var verticalVelocity = Vector3.Project(rb.linearVelocity, Vector3.up);
+        state.verticalSpeed = verticalVelocity.magnitude;
+        var horizontalVelocity = Vector3.Project(rb.linearVelocity, Vector3.Cross(Vector3.up, transform.right));
+        state.ldRatio = horizontalVelocity.magnitude / state.verticalSpeed;
+
         if (state.velocityMagnitude > 0.1f)
         {
             SimulateStabilizer();
@@ -56,6 +62,8 @@ public class PaperPlane : MonoBehaviour
         if (angle > 180f) angle -= 360f;
         Vector3 torque = axis * (angle * Mathf.Deg2Rad) * stabilizerStrength;
         rb.AddTorque(torque, ForceMode.Force);
+
+        rb.linearDamping = linearDumpWrtAngularVelocity.Evaluate(rb.angularVelocity.magnitude);
     }
     private void SimulateLift()
     {
@@ -75,5 +83,7 @@ public class PaperPlane : MonoBehaviour
         public Vector3 velocityNormalized;
         public float velocityMagnitude;
         public float fwdSpeed;
+        public float verticalSpeed;
+        public float ldRatio;
     }
 }
